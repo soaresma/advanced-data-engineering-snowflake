a87050b1bf4b4b3770a950af2a29cbf7bb48a3c9
soaresma <silveirama.soares@gmail.com>
2026-04-14 20:13:09 -0300

    Commit

--- module-1/api_integration.sql
+++ module-1/api_integration.sql
@@ -5,21 +5,21 @@ USE SCHEMA public;
 -- Create credentials
 CREATE OR REPLACE SECRET course_repo.public.github_pat
   TYPE = password
-  USERNAME = ''
-  PASSWORD = '';
+  USERNAME = 'soaresma'
+  PASSWORD = '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';
 
 -- Create the API integration
-CREATE OR REPLACE API INTEGRATION
+CREATE OR REPLACE API INTEGRATION git_api_integration
   API_PROVIDER = git_https_api
-  API_ALLOWED_PREFIXES = ('') -- URL to your GitHub profile
-  ALLOWED_AUTHENTICATION_SECRETS = ()
+  API_ALLOWED_PREFIXES = ('https://github.com/soaresma') -- URL to your GitHub profile
+  ALLOWED_AUTHENTICATION_SECRETS = (github_pat)
   ENABLED = TRUE;
 
 -- Create the git repository object
 CREATE OR REPLACE GIT REPOSITORY course_repo.public.advanced_data_engineering_snowflake
-  API_INTEGRATION =  -- Name of the API integration defined above
-  ORIGIN = '' -- Insert URL of forked repo
-  GIT_CREDENTIALS = ;
+  API_INTEGRATION = git_api_integration  -- Name of the API integration defined above
+  ORIGIN = 'https://github.com/soaresma/advanced-data-engineering-snowflake.git' -- Insert URL of forked repo
+  GIT_CREDENTIALS = course_repo.public.github_pat;
 
 -- List the git repositories
 SHOW GIT REPOSITORIES;
